ed6e56c9a3604aaf486f14a5cad8a3bd91812bb8
Mariusz Koziel <m.koziel@visuality.pl>
2015-11-24 23:24:02 +0100

    Refactoring and change to polish(Expedition pages)

--- app/controllers/expeditions_controller.rb
+++ app/controllers/expeditions_controller.rb
@@ -1,58 +1,59 @@
 class ExpeditionsController < ApplicationController
   before_action :set_expedition, only: [:show, :edit, :update, :destroy]
 
-  # GET /expeditions
   def index
     @expeditions = Expedition.all
   end
 
-  # GET /expeditions/1
   def show
   end
 
-  # GET /expeditions/new
   def new
     @expedition = current_user.expeditions.new
   end
 
-  # GET /expeditions/1/edit
   def edit
   end
 
-  # POST /expeditions
   def create
     @expedition = current_user.expeditions.new(expedition_params)
 
     if @expedition.save
-      redirect_to @expedition, notice: 'Expedition was successfully created.'
+      redirect_to @expedition, notice: _('Wyprawa została dodana')
     else
       render :new
     end
   end
 
-  # PATCH/PUT /expeditions/1
   def update
     if @expedition.update(expedition_params)
-      redirect_to @expedition, notice: 'Expedition was successfully updated.'
+      redirect_to @expedition, notice: _('Wyprawa została edytowana')
     else
       render :edit
     end
   end
 
-  # DELETE /expeditions/1
   def destroy
     @expedition.destroy
-    redirect_to expeditions_url, notice: 'Expedition was successfully destroyed.'
+    redirect_to expeditions_url, notice: _('Wyprawa została usunięta')
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_expedition
-      @expedition = Expedition.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def expedition_params
-      params.require(:expedition).permit(:start_at, :end_at, :place_id, :notes, :pressure, :showers, :overcast, :temperature)
-    end
+  def set_expedition
+    @expedition = Expedition.find(params[:id])
+  end
+
+  def expedition_params
+    params.require(:expedition).permit(
+      :start_at,
+      :end_at,
+      :place_id,
+      :notes,
+      :pressure,
+      :showers,
+      :overcast,
+      :temperature
+    )
+  end
 end
--- spec/factories/expeditions.rb
+++ spec/factories/expeditions.rb
@@ -1,13 +1,12 @@
 FactoryGirl.define do
   factory :expedition do
     start_at "2015-05-17 22:35:35"
-end_at "2015-05-17 22:35:35"
-place_id 1
-notes "MyText"
-pressure "MyString"
-showers "MyString"
-overcast "MyString"
-temperature 1
+    end_at "2015-05-17 22:35:35"
+    place_id 1
+    notes "MyText"
+    pressure "MyString"
+    showers "MyString"
+    overcast "MyString"
+    temperature 1
   end
-
 end
