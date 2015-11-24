196dbf29f0a6293f52216595ead568fbb72d004d
Mariusz Koziel <m.koziel@visuality.pl>
2015-11-24 22:14:12 +0100

    Add fish validation, change to polish, ckeditor

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -2,5 +2,6 @@ class Fish < ActiveRecord::Base
   has_many :expedition_fishes, dependent: :destroy
   has_many :expeditions, through: :expedition_fishes
 
-
+  validates :name, presence: true
+  validates :name, uniqueness: {case_sensitive: false}
 end
