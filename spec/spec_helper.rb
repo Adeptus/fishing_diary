a2f357d3149feca336df3c4917a92d5097f68e53
Mariusz Koziel <m.koziel@visuality.pl>
2016-01-02 21:11:52 +0100

    Add DatabaseCleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,25 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do |example|
+    if example.metadata[:js]
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+
   config.include FactoryGirl::Syntax::Methods
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
