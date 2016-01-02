58974846887ae8a93be8dcc5673171cf32ba8816
Mariusz Koziel <m.koziel@visuality.pl>
2016-01-02 16:02:35 +0100

    Add pl translations to DateTime helper

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,4 @@
-FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
-FastGettext.default_available_locales = ['en','de'] #all you want to allow
-FastGettext.default_text_domain = 'app'
+FastGettext.add_text_domain 'fishing_diary', :path => 'locale', :type => :po
+FastGettext.default_available_locales = ['pl', 'en'] #all you want to allow
+FastGettext.default_text_domain = 'fishing_diary'
+FastGettext.locale = 'pl'
